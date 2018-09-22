dd5e0b392cdf1228ad60290ed3d0577538863ade
Alexandra Carter <quietasasleepingarmy@Alexandras-MacBook-Air.local>
2018-09-22 13:48:07 -0700

    updated component and pages files

---
+++ client/src/pages/Courses/Courses.js
@@ -0,0 +1,48 @@
+import React, { Component } from "react";
+import Jumbotron from "../../components/Jumbotron";
+import Wrapper from "../../components/Wrapper";
+// import API from "../../utils/API";
+import { Col, Row, Container } from "../../components/Grid";
+// import { Input, TextArea, FormBtn } from "../../components/Form";
+
+class Courses extends Component {
+  state = {
+    courses: []
+  };
+
+  // componentDidMount() {
+  //   this.loadCourses();
+  // }
+
+  // loadCourses = () => {
+  //   API.getCourses()
+  //     .then(res => this.setState({ courses: res.data }))
+  //     .catch(err => console.log(err));
+  // };
+
+  render() {
+    return (
+     
+      <Wrapper>
+        <Jumbotron>
+      <h1>Wine'd Up! </h1>
+      </Jumbotron>
+              <Container fluid>
+
+          <Row>
+            <Col size="md-9">
+            </Col>
+            <Col size="md-6 sm-12">
+                <h1>Courses</h1>
+                
+                <h1>Exams</h1>
+
+            </Col>
+          </Row>
+        </Container>
+      </Wrapper>
+    );
+  }
+}
+
+export default Courses;
---
+++ client/src/pages/Login/Login.js
@@ -0,0 +1,29 @@
+// import React, { Component } from "react";
+// import Jumbotron from "../../components/Jumbotron";
+// import API from "../../utils/API";
+// import { Col, Row, Container } from "../../components/Grid";
+// import { Input, TextArea, FormBtn } from "../../components/Form";
+
+
+// render() {
+//     return (
+//         <Container fluid>
+//             <Row>
+//                 <Col size="md-6">
+//                     <Jumbotron>
+//                         <h1>Wine'd Up! </h1>
+//                     </Jumbotron>
+//                     <form>
+//                         <Input name="username" placeholder="Username (required)" />
+//                         <Input name="Password" placeholder="Password (required)" />
+//                         <FormBtn>Log In</FormBtn>
+//                     </form>
+
+//                 </Col>
+//             </Row>
+//         </Container>
+        
+//     );
+
+// }
+// export default Login;
