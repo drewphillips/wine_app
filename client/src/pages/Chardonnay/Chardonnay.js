e05f1928064d4e133b9069ee26366c8ef79aa65e
Alexandra Carter <quietasasleepingarmy@Alexandras-MacBook-Air.local>
2018-09-27 21:38:12 -0700

    front end changes

---
+++ client/src/pages/Chardonnay/Chardonnay.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+import Jumbotron from "../../components/Jumbotron";
+import Wrapper from "../../components/Wrapper";
+import { Col, Row, Container } from "../../components/Grid";
+import CourseCard from "../../components/CourseCard";
+import CarouselPage from "../../components/CarouselPage";
+import { Card, CardBody, CardImage, CardTitle, CardText, Fa } from 'mdbreact';
+
+
+
+
+
+
+class Chardonnay extends React.Component {
+
+  render() {
+  
+    return (
+        <div>
+
+          <CarouselPage>
+         
+            </CarouselPage>
+          
+          </div>
+    
+    );
+  }
+}
+
+export default Chardonnay;
--- client/src/pages/Login/Login.js
+++ client/src/pages/Login/Login.js
@@ -1,40 +1,40 @@
 import React, { Component } from "react";
+import "./login.css";
+
 import Jumbotron from "../../components/Jumbotron";
 // import API from "../../utils/API";
-import { Col, Row, Container } from "../../components/Grid";
-import { Input, TextArea, FormBtn } from "../../components/Form";
 import Wrapper from "../../components/Wrapper";
+import { Container, Row, Col, Input, Button } from 'mdbreact';
+
 
 
 
 class Login extends Component {
 
     render() {
-        return (
-
-      <Wrapper>
-      <Jumbotron>
-        <h1>Wine'd Up! </h1>
-      </Jumbotron>
-            
-            <Container fluid>
-                <Row>
-                    <Col size="md-6">
-                        <h1>Wine'd Up! </h1>
-
-                        <form>
-                            <Input name="username" placeholder="Username (required)" />
-                            <Input name="Password" placeholder="Password (required)" />
-                            <FormBtn>Log In</FormBtn>
-                        </form>
-
-
-                    </Col>
-                </Row>
-            </Container>
-            </Wrapper>
-
+        return(
+        <Wrapper>
+        <Jumbotron>
+          <Container>
+            <Row>
+              <Col md="6">
+                <form>
+                  <p className="h5 text-center mb-4">Sign in</p>
+                  <div className="grey-text">
+                    <Input label="Type your email" icon="envelope" group type="email" validate error="wrong" success="right"/>
+                    <Input label="Type your password" icon="lock" group type="password" validate/>
+                  </div>
+                  <div className="text-center">
+                    <Button>Login</Button>
+                  </div>
+                </form>
+              </Col>
+            </Row>
+          </Container>
+          </Jumbotron>
+        </Wrapper>
         );
-    }
-}
+      }
+    };
+    
 export default Login;
