51e855306c15492b64e1b14e3d50f8958d1f6aa5
Peter Christensen <christensenr98@yahoo.com>
2018-09-29 10:04:31 -0700

    hotlinked a courses/exams/grapes

--- client/src/pages/CourseNav/CourseNav.js
+++ client/src/pages/CourseNav/CourseNav.js
@@ -4,6 +4,7 @@ import Wrapper from "../../components/Wrapper";
 // import API from "../../utils/API";
 import { Col, Row, Container } from "../../components/Grid";
 // import { Input, TextArea, FormBtn } from "../../components/Form";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 class CourseNav extends Component {
     state = {
@@ -24,9 +25,9 @@ render() {
             <Col size="md-9">
             </Col>
             <Col size="md-6 sm-12">
-                <h1>White Grapes</h1>
+                <h1><OsLink to="/" label="White Grapes">White Grapes</OsLink></h1>
                 
-                <h1>Red Grapes</h1>
+                <h1><OsLink to="/" label="Red Grapes">Red Grapes</OsLink></h1>
 
             </Col>
           </Row>
@@ -35,5 +36,18 @@ render() {
     );
   }
 }
+
+const OsLink = ({ label, to, activeOnlyWhenExact }) => (
+  <Route
+    path={to}
+    exact={activeOnlyWhenExact}
+    children={({ match }) => (
+      <div className={match ? "active" : ""}>
+        {match ? " " : ""}
+        <Link to={to}>{label}</Link>
+      </div>
+    )}
+  />
+);
 
 export default CourseNav;
