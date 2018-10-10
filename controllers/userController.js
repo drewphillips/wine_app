d66cbbcb2f3255c3e2a0f6a5f79980543ad52f4b
Ivan Lewis <ivan.lewis@tonation-nsn.gov>
2018-10-10 14:51:40 -0700

    cleaned

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,16 +1,10 @@
 const db = require("../models");
 
 module.exports = {
-<<<<<<< HEAD
 
     findAllUsers: function(req, res) {
         db.user.findAll({}).then(function(dbUsers){
             res.json(dbUsers);
-=======
-    createUser: function (req, res) {
-        db.user.create(req.body).then(function (dbUser) {
-            res.json(dbUser);
->>>>>>> master
         });
     },
 
@@ -23,21 +17,11 @@ module.exports = {
             res.json(dbUser)
         });
     },
-    //   test route for post man 
-    //   http://localhost:3000/api/user/allusers
 
-<<<<<<< HEAD
     createUser: function(req,res){
         db.user.create(req.body).then(function(dbUser) {
             res.json(dbUser);
           });
     },
-=======
-    findAllUsers: function (req, res) {
-        db.user.findAll({}).then(function (dbUsers) {
-            res.json(dbUsers);
-        });
-    }
->>>>>>> master
 
 };
